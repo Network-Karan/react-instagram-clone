382334f06ddd173ff2bbc39840c31e5e40a84071
Karan <kaliyaperumal.diwakaran@gmail.com>
2022-06-03 12:59:02 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import styled from 'styled-components'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App"></div>
+    <Router>
+      <Container>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
+
+const Container = styled.div``;
 
 export default App;
